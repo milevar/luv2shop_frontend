f8a6a63a2cddf55f7e9e034ee443930e3cff8fd8
Milena Ramirez Lopez <milena.ramirez.lopez@gmail.com>
2021-10-11 15:09:26 -0400

    14.- Search for Products by Keyword

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -12,6 +12,7 @@ export class ProductListComponent implements OnInit {
 
   products: Product[];
   currentCategoryId: number;
+  searchMode: boolean;
 
   constructor(private productService: ProductService, private route: ActivatedRoute) { }
 
@@ -22,6 +23,24 @@ export class ProductListComponent implements OnInit {
   }
 
   listProducts() {
+    this.searchMode = this.route.snapshot.paramMap.has('keyword');
+    if (this.searchMode) {
+      this.handleSearchProducts();
+    } else {
+      this.handleListProducts();
+    }
+
+  }
+
+  handleSearchProducts () {
+    const theKeyword: string = this.route.snapshot.paramMap.get('keyword');
+    this.productService.searchProducts(theKeyword).subscribe(
+      data => {
+        this.products = data;
+      }
+    );
+  }
+  handleListProducts() {
     // check if id parameter is available
     const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
     if (hasCategoryId) {
@@ -31,7 +50,7 @@ export class ProductListComponent implements OnInit {
     }
     this.productService.getProductList(this.currentCategoryId).subscribe(
       data => {
-         this.products = data;
+        this.products = data;
       }
     );
   }
